d000aaea55f7d5561f06dfdfd300a8bff4582c7e
MaryanaRashkevych <149114863+MaryanaRashkevych@users.noreply.github.com>
2024-01-21 23:10:49 +0200

    homework2

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,23 @@
-/*Оголоси функцію getShippingMessage, яка очікує три параметри, значення яких будуть задаватися під час її виклику:
+// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
 
-country — перший параметр, рядок, що містить країну доставки
-price — другий параметр, число, що містить загальну вартість товару
-deliveryFee — третій параметр, число, що містить вартість доставки товару
-Доповни код функції так, щоб вона повертала рядок з повідомленням про доставку товару в країну користувача: "Shipping to <country> will cost <totalPrice> credits", де:
+// Доповни код функції таким чином, що:
 
-<country> — це країни доставки
-<totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки
-Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.*/
+// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
+// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  const msg = `Shipping to ${country} will cost ${totalPrice} credits.`;
-  return msg;
+function formatMessage(message, maxLength) {
+  if (message.lenght <= maxLength) {
+    return message;
+  } else {
+    const newMsg = message.slice(0, maxLength) + '...';
+    return newMsg;
+  }
 }
 
-console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage('Sweden', 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
+console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
+console.log(formatMessage('Vestibulum facilisis purus nec', 20)); // "Vestibulum facilisis..."
+console.log(formatMessage('Vestibulum facilisis purus nec', 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15)); // "Nunc sed turpis..."
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41)); // "Nunc sed turpis a
